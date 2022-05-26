71813d4e568370e60c2d02a086e75777b495c856
delee5695 <delee5695@naver.com>
2022-05-26 18:09:51 +0900

    css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import logoimg from './logo2.png'
+import { slide as Menu } from 'react-burger-menu'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="logo2.png"></img>
-        <h1>Our Hearts in Korea, Our Eyes to the World</h1>
-      </header>
+      
+      <div className="App-header">
+        <img src={logoimg} className="logoimg" height="90px"/>
+        <h1>Our Hearts in Korea,<br/> Our Eyes to the World</h1>
+      </div>
+      <Menu>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+
+      <a className="menu-item" href="/laravel">
+        Laravel
+      </a>
+
+      <a className="menu-item" href="/angular">
+        Angular
+      </a>
+
+      <a className="menu-item" href="/react">
+        React
+      </a>
+
+      <a className="menu-item" href="/vue">
+        Vue
+      </a>
+
+      <a className="menu-item" href="/node">
+        Node
+      </a>
+    </Menu>
+      
     </div>
-  )
+  );
 }
 
 export default App;
